6cf32d343b110bc875bb576bdb0c6c835635ea98
Kwabena Ampofo <amp.kwabena@gmail.com>
2025-10-21 00:05:25 -0400

    Specify return types on handlers

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import { config } from "../config.js";
 
-export async function handlerMetrics(_: Request, res: Response) {
+export async function handlerMetrics(_: Request, res: Response): Promise<void> {
   res.send(`Hits: ${config.fileServerHits}`);
 }
--- src/api/reset.ts
+++ src/api/reset.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import { config } from "../config.js";
 
-export async function handlerReset(_: Request, res: Response) {
+export async function handlerReset(_: Request, res: Response): Promise<void> {
   config.fileServerHits = 0;
   res.send();
 }
