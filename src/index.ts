da0eddf9c68b2af1656a2d7f33251f0b50ece34e
Kwabena Ampofo <amp.kwabena@gmail.com>
2025-11-17 09:50:04 -0500

    CH5: Error Handling (#2)

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,36 @@ import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
 
+import { handlerChirpsValidate } from "./api/chirps.js";
 import {
-  middlewareLogResponses,
+  errorMiddleWare,
+  middlewareLogResponse,
   middlewareMetricsInc,
 } from "./api/middlware.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(middlewareLogResponses);
+app.use(middlewareLogResponse);
+app.use(express.json());
+
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", (req, res, next) => {
+  Promise.resolve(handlerReadiness(req, res)).catch(next);
+});
+app.get("/admin/metrics", (req, res, next) => {
+  Promise.resolve(handlerMetrics(req, res)).catch(next);
+});
+app.post("/admin/reset", (req, res, next) => {
+  Promise.resolve(handlerReset(req, res)).catch(next);
+});
 
-app.get("/metrics", handlerMetrics);
+app.post("/api/validate_chirp", (req, res, next) => {
+  Promise.resolve(handlerChirpsValidate(req, res)).catch(next);
+});
 
-app.get("/reset", handlerReset);
+app.use(errorMiddleWare);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
